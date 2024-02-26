3b1cec67f56567fa43ca3486609864ae633a51a7
BoyChai <1972567225@qq.com>
2024-02-26 23:28:01 +0800

    feat(route): hello request routing

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("hello,squirrel")
+	r := gin.Default()
+	r.Any("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"code": 200,
+			"msg":  "Hello,Squirrel",
+			"data": "https://github.com/boychai/squirrel-ce",
+		})
+	})
+	r.Run()
 }
